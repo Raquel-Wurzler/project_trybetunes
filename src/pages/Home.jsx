783e0590c94ca30d6e3f6321b26d561d38b52bf8
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-01 16:19:47 -0300

    Question One Done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        Home
+      </div>
+    );
+  }
+}
+
+export default Home;
