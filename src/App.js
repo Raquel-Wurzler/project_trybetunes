29891233bb88636a034868d73470a5a4a143606f
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-01 19:57:11 -0300

    Question Two Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Loading from './components/Loading';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
+        <Route exact path="/" component={ Login } />
         <Route exact path="/search" component={ Search } />
         <Route exact path="/album/:id" component={ Album } />
         <Route exact path="/favorites" component={ Favorites } />
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <div>
-          <Route component={ NotFound } />
-        </div>
+        <Route component={ NotFound } />
+        <Route exact path="/loading" component={ Loading } />
       </BrowserRouter>
     );
   }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
+
+const NAME_LENGTH = 3;
+
+class Login extends Component {
+  state = {
+    btnDisabled: true,
+    nameInput: '',
+    loading: false,
+  }
+
+  loginChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => {
+      const { nameInput } = this.state;
+      const btnIsDisabled = nameInput.length < NAME_LENGTH;
+      this.setState({
+        btnDisabled: btnIsDisabled,
+      });
+    });
+  }
+
+  btnSaveOnClick = async () => {
+    this.setState({ loading: true });
+    const { nameInput } = this.state;
+    const { history } = this.props;
+    await createUser({ name: nameInput });
+    history.push('/search');
+  }
+
+  render() {
+    const { btnDisabled, loading } = this.state;
+
+    const form = (
+      <form>
+        <input
+          type="text"
+          name="nameInput"
+          id="name"
+          data-testid="login-name-input"
+          onChange={ this.loginChange }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ btnDisabled }
+          onClick={ this.btnSaveOnClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+
+    return (
+      <div data-testid="page-login">
+        {
+          loading ? <Loading /> : form
+        }
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default Login;
