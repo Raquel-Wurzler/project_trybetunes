8c4bc182168ac195dac2378f04b0e4cce88665e2
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-01 22:04:47 -0300

    Trying Question Five

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const BAND_NAME_LENGTH = 2;
+
 class Search extends Component {
+  state = {
+    btnIsDisabled: true,
+    bandName: '',
+  }
+
+  bandNameChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => {
+      const { bandName } = this.state;
+      const btnForDisabled = bandName.length < BAND_NAME_LENGTH;
+      this.setState({
+        btnIsDisabled: btnForDisabled,
+      });
+    });
+  }
+
   render() {
+    const { btnIsDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.bandNameChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ btnIsDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
