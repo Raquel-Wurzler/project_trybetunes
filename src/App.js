9a78a29e8d7e30cc5409a6bacc1c9473623f12a2
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-02 14:28:46 -0300

    Question Six Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Search from './pages/Search';
 import Album from './pages/Album';
-import Favorites from './pages/Favorites';
+import Search from './pages/Search';
 import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
 import Loading from './components/Loading';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route component={ NotFound } />
-        <Route exact path="/loading" component={ Loading } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/loading" component={ Loading } />
+          <Route component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const BAND_NAME_LENGTH = 2;
 
 class Search extends Component {
   state = {
     bandName: '',
+    loading: false,
+    albuns: [],
+    artistName: '',
   }
 
   bandNameChange = (event) => {
@@ -14,26 +20,76 @@ class Search extends Component {
     });
   }
 
-  render() {
+  btnClickClearAndRequest = async () => {
     const { bandName } = this.state;
+    const bandList = await searchAlbumsAPI(bandName);
+    this.setState({ loading: true, albuns: [...bandList] }, () => {
+      this.setState({
+        loading: false,
+        artistName: bandName,
+        bandName: '',
+      });
+    });
+  }
+
+  render() {
+    const { bandName, loading, albuns, artistName } = this.state;
     const buttonDis = bandName.length < BAND_NAME_LENGTH;
+    const form = (
+      <form>
+        <input
+          type="text"
+          name="bandName"
+          value={ bandName }
+          data-testid="search-artist-input"
+          onChange={ this.bandNameChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ buttonDis }
+          onClick={ this.btnClickClearAndRequest }
+        >
+          Pesquisar
+        </button>
+      </form>
+    );
+
+    const notAlbunSearch = <h3>Nenhum álbum foi encontrado</h3>;
+    const mapInAlbuns = albuns.map((alb, i) => (
+      <div key={ i }>
+        <img src={ alb.artworkUrl100 } alt={ alb.artistName } />
+        <Link
+          to={ `/album/${alb.collectionId}` }
+          data-testid={ `link-to-album-${alb.collectionId}` }
+        >
+          Detalhes Álbum
+        </Link>
+        <p>{ alb.collectionName }</p>
+        <p>{ alb.artistName }</p>
+      </div>
+    ));
+    console.log(mapInAlbuns);
+
+    const foundAlbum = <h3>{`Resultado de álbuns de: ${artistName}`}</h3>;
+    const compareIfAlbun = albuns.length === 0 ? notAlbunSearch : mapInAlbuns;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.bandNameChange }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ buttonDis }
-          >
-            Pesquisar
-          </button>
-        </form>
+        <span>
+          { form }
+        </span>
+        <span>
+          {
+            !loading && foundAlbum
+          }
+        </span>
+        <span>
+          {
+            loading ? <Loading /> : compareIfAlbun
+          }
+        </span>
       </div>
     );
   }
