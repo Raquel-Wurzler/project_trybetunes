edfaebf3e7694878c71e9def17b73e75e650a749
Raquel_Gomes <raquel.wurzler@hotmail.com>
2022-08-01 21:07:42 -0300

    Question Four Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Header extends Component {
-    state ={
-      userName: '',
+  state ={
+    userName: '',
+    loading: false,
+  }
+
+  componentDidMount() {
+    this.captureName();
+  }
+
+  captureName = async () => {
+    this.setState({
+      loading: true,
+    });
+    const userObj = await getUser();
+    this.setState({
+      userName: userObj,
       loading: false,
-    }
+    });
+  }
 
-    componentDidMount() {
-      this.captureName();
-    }
-
-    captureName = async () => {
-      this.setState({
-        loading: true,
-      });
-      const userObj = await getUser();
-      this.setState({
-        userName: userObj,
-        loading: false,
-      });
-    }
-
-    render() {
-      const { loading, userName } = this.state;
-      const elementUser = (
-        <h1 data-testid="header-user-name">
-          {`Olá ${userName.name}!!`}
-        </h1>);
-      return (
-        <header data-testid="header-component">
-          { loading ? <Loading /> : elementUser }
-        </header>
-      );
-    }
+  render() {
+    const { loading, userName } = this.state;
+    const elementUser = (
+      <h1 data-testid="header-user-name">
+        {`Olá ${userName.name}!!`}
+      </h1>);
+    return (
+      <header data-testid="header-component">
+        { loading ? <Loading /> : elementUser }
+        <nav>
+          <ul>
+            <li><Link data-testid="link-to-search" to="/search"> Search </Link></li>
+            <li><Link data-testid="link-to-favorites" to="/favorites">Favorite</Link></li>
+            <li><Link data-testid="link-to-profile" to="/profile"> Profile </Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
